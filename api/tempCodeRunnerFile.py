0809c3e4db74e9c6f03cf472106e074af627c907
aryan-gupta-jiit <aryangupta.jiit@gmail.com>
2025-08-18 20:35:08 +0530

    react mobile app

--- api/tempCodeRunnerFile.py
+++ api/tempCodeRunnerFile.py
@@ -36,7 +36,6 @@ async def ping():
 async def predict(
     file:UploadFile = File(...),
 ):
-    # print(file.split(".")[-1])
     image=read_file_as_image(await file.read())
     img_batch=np.expand_dims(image, axis=0)
     predictions=MODEL.predict(img_batch)
@@ -49,4 +48,4 @@ async def predict(
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app,host="192.168.1.38",port=8080)
+    uvicorn.run(app,host="localhost",port=8080)
